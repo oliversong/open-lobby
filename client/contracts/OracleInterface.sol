92c7af48b783cb966cd1c34ef7194470d12fe61c
Oliver Song <me@oliversong.net>
2022-06-13 16:13:58 -0700

    recompile, bug bash

--- client/contracts/OracleInterface.sol
+++ client/contracts/OracleInterface.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract OracleInterface {
+abstract contract OracleInterface {
     enum BillOutcome {
         Pending,
         BecameLaw,
         TimedOut
     }
 
-    function getPendingBills() public view returns (bytes32[] memory);
+    function getPendingBills() public virtual view returns (bytes32[] memory);
 
-    function getAllBills() public view returns (bytes32[] memory);
+    function getAllBills() public virtual view returns (bytes32[] memory);
 
-    function billExists(bytes32 _billId) public view returns (bool);
+    function billExists(bytes32 _billId) public virtual view returns (bool);
 
-    function getBill(bytes32 _billId) public view returns (
+    function getBill(bytes32 _billId) public virtual view returns (
         bytes32 id,
         string memory sponsor,
         uint dateOfIntroduction,
@@ -22,13 +22,13 @@ contract OracleInterface {
         string memory legislationNumber,
         BillOutcome outcome);
 
-    function getAdditionalBillInfo(bytes32 _billId) public view returns (
+    function getAdditionalBillInfo(bytes32 _billId) public virtual view returns (
         string memory amendsBill,
         string memory committees,
         string memory latestAction,
         uint latestActionDate);
 
-    function getMostRecentBill(bool _pending) public view returns (
+    function getMostRecentBill(bool _pending) public virtual view returns (
         bytes32 id,
         string memory sponsor,
         uint dateOfIntroduction,
@@ -36,7 +36,7 @@ contract OracleInterface {
         string memory legislationNumber,
         BillOutcome outcome);
 
-    function testConnection() public pure returns (bool);
+    function testConnection() public virtual pure returns (bool);
 
-    function addTestData() public;
+    function addTestData() public virtual;
 }
